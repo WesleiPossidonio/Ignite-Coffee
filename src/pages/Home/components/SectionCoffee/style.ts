7bccf0aa72448b318785effe7289158ffc125ed1
Weslei França <weslwifranca@gmail.com>
2023-05-11 14:57:31 -0300

    update

--- src/pages/Home/components/SectionCoffee/style.ts
+++ src/pages/Home/components/SectionCoffee/style.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const ContainerSectionCoffee = styled.section`
-  margin-top: 6rem;
+  margin-top: 7rem;
 `
 
 export const TitleSection = styled.h1`
